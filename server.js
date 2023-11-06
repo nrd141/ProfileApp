ca304ab148dbbf1781c93e68ff0610ef2ffe8bdd
nrd141 <nrd141@gmail.com>
2023-11-06 08:33:55 +0700

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html');
+    res.sendFile(__dirname + '/index.html');
 });
 
 const PORT = process.env.PORT || 8080;
